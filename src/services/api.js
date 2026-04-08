344e4989754742e43e9e74b667c8ce27f26cc888
Nittu <nitturajkumar123@gmail.com>
2026-04-08 12:37:36 +0530

    Solution

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://telemedicine-server-1-aci0.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
